eb3d93d80b0cb6660313766123ae7e871e1e0030
Himanshu Dhiman <dhimanhimanshu26@gmail.com>
2024-01-17 01:12:37 +0530

    Update percentage_of_users_attended_contest.sql

--- Perentage-Of-Users-Attended-A-Contest/percentage_of_users_attended_contest.sql
+++ Perentage-Of-Users-Attended-A-Contest/percentage_of_users_attended_contest.sql
@@ -1,8 +1,8 @@
 /* Write your PL/SQL query statement below */
 SELECT r.contest_id,
          ROUND (
-             (COUNT (r.user_id) / (SELECT COUNT (*) FROM xxbackup.users)) * 100,
+             (COUNT (r.user_id) / (SELECT COUNT (*) FROM users)) * 100,
              2)    percentage
-    FROM xxbackup.register r
+    FROM register r
 GROUP BY contest_id
 ORDER BY percentage DESC, contest_id;
