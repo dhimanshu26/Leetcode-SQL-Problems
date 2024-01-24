2c72fb03b3d8d75f99efa73c8d87d6e758bab31e
Himanshu Dhiman <dhimanhimanshu26@gmail.com>
2024-01-24 15:39:25 +0530

    Create employees_whose_managers_left_the_company.sql

---
+++ Employees-Whose-Managers-Left-The-Company/employees_whose_managers_left_the_company.sql
@@ -0,0 +1,6 @@
+/* Write your PL/SQL query statement below */
+SELECT employee_id
+    FROM employees
+   WHERE     manager_id NOT IN (SELECT employee_id FROM employees)
+         AND salary < 30000
+ORDER BY employee_id;
