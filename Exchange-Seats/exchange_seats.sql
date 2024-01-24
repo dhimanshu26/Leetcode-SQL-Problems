c6bc444efa901675b23f72c24236492e792bf929
Himanshu Dhiman <dhimanhimanshu26@gmail.com>
2024-01-24 16:39:35 +0530

    Create exchange_seats.sql

---
+++ Exchange-Seats/exchange_seats.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+SELECT id,
+         NVL (
+             CASE
+                 WHEN MOD (id, 2) <> 0 THEN LEAD (student) OVER (ORDER BY id)
+                 ELSE LAG (student) OVER (ORDER BY id)
+             END,
+             student)    AS student
+    FROM xxbackup.seat
+ORDER BY id;
